946cfc6d9a6b81c40646c85238ef6a392992817e
Muthupriyadharshini M <dharshini199812@gmail.com>
2020-10-23 12:32:16 +0530

    Delete client.py

--- client.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-id = int(input("Enter the ID of the employee whose data you want to see"))
-
-params = {"id":id}
-get_data = requests.get(url='http://127.0.0.1:8080',params=params)
-print((get_data.headers))
-print(get_data.text)
