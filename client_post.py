81e385088dc3951a834fb1b0a0ff4e83847b3632
Muthupriyadharshini M <dharshini199812@gmail.com>
2020-10-23 10:57:33 +0530

    Update client_post.py

--- client_post.py
+++ client_post.py
@@ -2,9 +2,9 @@ import requests
 import json
 
 data = {}
-data['Name'] = 'Priya'
+data['Name'] = 'John Statham'
 data['Designation'] = 'Associate Developer'
-data['Status'] = 'Active'
+data['Status'] = 'inactive'
 jdata = json.dumps(data)
 send = requests.post(url='http://127.0.0.1:8080',json=jdata)
 print((send.headers))
