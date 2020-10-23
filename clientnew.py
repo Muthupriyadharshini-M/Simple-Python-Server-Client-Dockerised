fb5138167d5e2a83fc65c5623871698025e43c63
Muthupriyadharshini M <dharshini199812@gmail.com>
2020-10-23 12:34:22 +0530

    Create clientnew.py

---
+++ clientnew.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+
+url = "http://" + "nginxserver" + ":80"
+
+#answer = int(input("Enter 1 for a GET request and 2 for a POST request"))
+
+#id = int(input("Enter the ID of the employee whose data you want to see"))
+params = {"id":5}
+get_data = requests.get(url=url,params=params)
+print((get_data.headers))
+print(get_data.text)
+
+
+data = {}
+data['Name'] = "Hawking"
+data['Designation'] = "Site Head"
+data['Status'] = "active"
+jdata = json.dumps(data)
+send = requests.post(url=url,json=jdata)
+print((send.headers))
